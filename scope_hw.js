8b2278c00272a7283b7c06a127046df3b9ff2375
Rumen Roger <rumenroger@gmail.com>
2020-06-15 22:20:07 +0100

    Episode 1

---
+++ scope_hw.js
@@ -0,0 +1,16 @@
+// MVP
+// Episode 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
