4c52a97489244a2a2a272c89d029c1dc123f0235
Rumen Roger <rumenroger@gmail.com>
2020-06-15 22:23:32 +0100

    Episode 2

--- scope_hw.js
+++ scope_hw.js
@@ -14,3 +14,30 @@ const declareMurderer = function() {
 const verdict = declareMurderer();
 console.log(verdict);
 
+// PREDICTED OUTPUT #1 - The murderer is Miss Scarlet.
+// The [verdict] const variable (through the [declareMureder] function) 
+// can access the value 'Miss Scarlet' 
+// within the globally defined [scenario] object.
+
+
+// Episode 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// PREDICTED OUTPUT #2 - SyntaxError
+// The const variable [murderer] cannot be redefined.
+// Hence, the [verdict] const variable (through the [declareMurderer] function)
+// cannot access the new string of the [changeMurderer] function 
+// neither its calling can become effective. 
+
